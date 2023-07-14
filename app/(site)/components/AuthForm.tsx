02dedf3520bcd52fd70e9b022f2b6facd381f52e
Nikhil <nikhilchhillar29@gmail.com>
2023-07-14 22:19:28 +0530

    added sidebar

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,21 +2,33 @@
 
 import Button from "@/components/Button";
 import Input from "@/components/inputs/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import AuthBtn from "./AuthBtn";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
   //
 
+  const session = useSession();
+  const router = useRouter();
+
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      console.log("authenticated");
+
+      router.push("/users");
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -44,6 +56,22 @@ const AuthForm = () => {
       //
       axios
         .post("/api/register", data)
+        .then(() =>
+          signIn("credentials", {
+            ...data,
+            redirect: false,
+          })
+        )
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials...");
+          }
+
+          if (callback?.ok && !callback.error) {
+            toast.success("Successfully logged in...");
+            router.push("/users");
+          }
+        })
         .catch(() => toast.error("Something went wrong..."))
         .finally(() => setIsLoading(false));
       //
@@ -62,6 +90,7 @@ const AuthForm = () => {
 
           if (callback?.ok && !callback.error) {
             toast.success("Successfully logged in...");
+            router.push("/users");
           }
         })
         .finally(() => setIsLoading(false));
@@ -81,6 +110,7 @@ const AuthForm = () => {
 
         if (callback?.ok && !callback.error) {
           toast.success("Successfully logged in...");
+          router.push("/users");
         }
       })
       .finally(() => setIsLoading(false));
